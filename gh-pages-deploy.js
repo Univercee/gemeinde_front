f02a959f574c53133e2d85fbe0b3f047c352829e
Aleksandr Ostromogilskii <ostromog@mail.ru>
2022-10-28 15:31:31 +0300

    remove cname file creation

--- gh-pages-deploy.js
+++ gh-pages-deploy.js
@@ -11,8 +11,8 @@ const fs = require("fs");
     // await execa("yarn", ["build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
-    const hostName = "copy.gemeindeonline.ch";
-    fs.writeFile(folderName + '/CNAME', hostName, function(err) {});
+    //const hostName = "copy.gemeindeonline.ch";
+    //fs.writeFile(folderName + '/CNAME', hostName, function(err) {});
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
