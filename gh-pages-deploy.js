abfda4d6cb5290e8dd0f26a6c67a9c0e79761dd6
Aleksandr Ostromogilskii <ostromog@mail.ru>
2022-10-28 15:09:42 +0300

    change hostname

--- gh-pages-deploy.js
+++ gh-pages-deploy.js
@@ -11,7 +11,7 @@ const fs = require("fs");
     // await execa("yarn", ["build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
-    const hostName = "dev.gemeindeonline.ch";
+    const hostName = "dev.copy.gemeindeonline.ch";
     fs.writeFile(folderName + '/CNAME', hostName, function(err) {});
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
